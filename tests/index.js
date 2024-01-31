d9f1810b9b14d4c90dc681dffb66da86f6e7659c
Werner Glinka <werner@glinka.co>
2024-01-31 14:06:24 -0800

    converted to es6

--- tests/index.js
+++ tests/index.js
@@ -2,63 +2,69 @@
 
 'use strict';
 
-const chai = require('chai');
-const metalsmith = require('metalsmith');
-const metalsmithLinks = require('../lib');
-const fs = require('fs');
-const path = require('path');
-const expect = chai.expect;
+import * as chai from 'chai';
+import metalsmith from 'metalsmith';
+import metalsmithLinks from '../lib/index.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 
-const fixture = path.resolve.bind(path, __dirname, 'fixtures/markup');
+/* eslint-disable no-underscore-dangle */
+const __dirname = dirname( fileURLToPath( import.meta.url ) );
 
-function file(_path) {
-  return fs.readFileSync(fixture(_path), 'utf8');
+const { expect } = chai;
+
+const fixture = path.resolve.bind( path, __dirname, 'fixtures/markup' );
+
+function file( _path ) {
+  return fs.readFileSync( fixture( _path ), 'utf8' );
 }
 
-describe('metalsmith-prism', () => {
+describe( 'metalsmith-prism', () => {
 
-  it('should strip host name from local links href', done => {
+  it( 'should strip host name from local links href', done => {
 
-    const metal = metalsmith(fixture());
-
-    metal
-      .use(metalsmithLinks({
-          hostnames: ["www.potatohead.com"]
-        }))
-      .build( err => {
-
-        if (err) {
-          return done(err);
-        }
-
-        expect(file('build/local-link.html')).to.be.eql(file('expected/local-link.html'));
-        
-
-        done();
-      });
-
-  });
-
-  it('should add target and rel attributes to external links', done => {
-
-    const metal = metalsmith(fixture());
+    const metal = metalsmith( fixture() );
 
     metal
-      .use(metalsmithLinks({
-          hostnames: ["www.potatohead.com"]
-        }))
+      .use( metalsmithLinks( {
+        hostnames: [ "www.potatohead.com" ]
+      } ) )
       .build( err => {
 
-        if (err) {
-          return done(err);
+        if ( err ) {
+          return done( err );
         }
 
-        expect(file('build/external-link.html')).to.be.eql(file('expected/external-link.html'));
-        
+        expect( file( 'build/local-link.html' ) ).to.be.eql( file( 'expected/local-link.html' ) );
+
 
         done();
-      });
+      } );
 
-  });
+  } );
 
-});
+  it( 'should add target and rel attributes to external links', done => {
+
+    const metal = metalsmith( fixture() );
+
+    metal
+      .use( metalsmithLinks( {
+        hostnames: [ "www.potatohead.com" ]
+      } ) )
+      .build( err => {
+
+        if ( err ) {
+          return done( err );
+        }
+
+        expect( file( 'build/external-link.html' ) ).to.be.eql( file( 'expected/external-link.html' ) );
+
+
+        done();
+      } );
+
+  } );
+
+} );
